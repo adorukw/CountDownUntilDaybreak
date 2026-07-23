5ceaf4fd39b74913740a35d6418c98e82bf5090f
adorukw <kw2261387@outlook.com>
2026-07-23 14:36:13 +0800

    初次提交

---
+++ README.md
@@ -0,0 +1,3 @@
+# CounDown: UntilDaybreak(倒数：直至破晓)
+
+> 参加2026年GMTK game jam（之一）
---
+++ build/CMakeFiles/CDUD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CDUD.dir/link.d"
+  "CDUD"
+  "CDUD.pdb"
+  "CMakeFiles/CDUD.dir/src/main.c.o"
+  "CMakeFiles/CDUD.dir/src/main.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CDUD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
