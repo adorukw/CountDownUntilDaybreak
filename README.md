b36dc3d499a87749b1d5b1583a9d6a592222fdd5
adorukw <kw2261387@outlook.com>
2026-07-23 23:12:40 +0800

    整理素材图&绘制地图&安装cJSON

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # CounDown: UntilDaybreak(倒数：直至破晓)
 
 > 参加2026年GMTK game jam（之一）
+
+## 一. 开发笔记
+
+### 1. 解析tmj文件
+
+#### 1.1 安装cJSON库
+```bash
+
+```
---
+++ assets/Tiled/props.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="props" tilewidth="76" tileheight="321" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="../images/props/platform2.png" width="48" height="21"/>
+ </tile>
+ <tile id="6">
+  <image source="../images/props/pillar1.png" width="64" height="112"/>
+ </tile>
+ <tile id="7">
+  <image source="../images/props/platform3.png" width="44" height="11"/>
+ </tile>
+ <tile id="8">
+  <image source="../images/props/platform1.png" width="46" height="13"/>
+ </tile>
+ <tile id="3">
+  <image source="../images/props/pillar2.png" width="76" height="164"/>
+ </tile>
+ <tile id="5">
+  <image source="../images/props/glow.png" width="60" height="61"/>
+ </tile>
+ <tile id="9">
+  <image source="../images/props/pillar3.png" width="35" height="321"/>
+ </tile>
+ <tile id="10">
+  <image source="../images/props/hang_light.png" width="60" height="228"/>
+ </tile>
+</tileset>
